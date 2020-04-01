f61dfc7938895f5d697a68fb10fa08600232ff13
Forkeep <13126593533@163.com>
2020-04-01 23:31:11 +0800

    parcel 资源地址配置

--- dist/reset.302ca0b1.js
+++ dist/reset.302ca0b1.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63412" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64977" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -393,4 +393,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/reset.302ca0b1.js.map
+//# sourceMappingURL=reset.302ca0b1.js.map
